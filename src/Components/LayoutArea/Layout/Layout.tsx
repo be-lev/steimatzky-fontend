773814fd02c80c7d5d1fece4d253a616ec8d094b
Lev <levberger19890@gmail.com>
2021-01-29 13:52:49 +0200

    first commit

---
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Layout.css";
+
+function Layout(): JSX.Element {
+    return (
+        <div className="Layout">
+			
+        </div>
+    );
+}
+
+export default Layout;
