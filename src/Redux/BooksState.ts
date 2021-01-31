06cb928356d03f2d9431a786fcf11848e9e8b71e
Lev <levberger19890@gmail.com>
2021-01-31 11:02:02 +0200

    create Redux system

---
+++ src/Redux/BooksState.ts
@@ -0,0 +1,38 @@
+import BookModel from "../Components/BooksArea/Models/BooksModel";
+
+export class BooksState {
+    public books: BookModel[] = [];
+}
+
+export enum BooksActionType {
+    BooksDownloaded,
+    BookAdded
+}
+
+export interface BooksAction {
+    type: BooksActionType;
+    payload?: any;
+}
+
+export function BookDownloadedAction(books: BookModel[]): BooksAction {
+    return { type: BooksActionType.BooksDownloaded, payload: books }
+}
+
+export function BookAddedAction(book: BookModel[]): BooksAction {
+    return { type: BooksActionType.BookAdded, payload: book }
+}
+
+export function BooksReducer(currentState: BooksState = new BooksState(), action: BooksAction): BooksState{
+    const newState = {...currentState};
+
+    switch ( action.type) {
+
+        case BooksActionType.BooksDownloaded:
+            newState.books=action.payload;
+            break;
+        case BooksActionType.BookAdded:
+            newState.books.push(action.payload);
+            break;
+    }
+    return newState;
+}
